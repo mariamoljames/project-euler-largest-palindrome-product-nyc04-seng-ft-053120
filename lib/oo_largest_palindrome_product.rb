2e5a6a3092b7538c9a17109d3843ba4777425a77
mariamoljames <mariamol.james@gmail.com>
2020-12-10 13:23:23 -0500

    Done.

--- lib/oo_largest_palindrome_product.rb
+++ lib/oo_largest_palindrome_product.rb
@@ -1,1 +1,30 @@
-# Implement your object-oriented solution here!
+class LargestPalindromeProduct
+    attr_accessor :max
+    def initialize
+        @max=answer
+    end
+    def answer
+    @max=0
+    def isPalindrome(n)
+        str=n.to_s
+        left=0
+        right=str.length-1
+        while left<right
+            if str[left]!=str[right]
+                return false
+            end
+            left+=1
+            right-=1
+        end
+        true
+    end
+    for i in (999).downto(100)
+        for j in (i).downto(100)
+            if isPalindrome(i*j) && (i*j)>max
+                @max=i*j
+            end
+        end
+    end
+    @max
+end
+end
